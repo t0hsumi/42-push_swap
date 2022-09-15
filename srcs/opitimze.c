708a254b3bff499c9f5ece149ea6c6e044f8caea
Taito Ohsumi <ohsumi-taito035@g.ecc.u-tokyo.ac.jp>
2022-09-15 12:37:11 +0900

    make the sequence of instruction ra->rb to rr

--- srcs/opitimze.c
+++ srcs/opitimze.c
@@ -1,6 +1,36 @@
 #include <push_swap.h>
 
-static void to_rr(stacks_t *s) {
+static void ra_rb(stacks_t *s) {
+	dlnode_t *cur = s->solution->dummy->next;
+	enum instruction RR = rr;
+
+	while (cur != s->solution->dummy) {
+		if (*(enum instruction *)cur->data == ra) {
+			int flag = 0;
+			dlnode_t *tmp = cur->next;
+			while (tmp != s->solution->dummy) {
+				enum instruction inst = *(enum instruction *)tmp->data;
+				if (inst == rb) {
+					flag = 1;
+					break;
+				}
+				else if (inst == sa || inst == ra || inst == rra){
+					tmp = tmp->next;
+					continue;
+				}
+				else
+					break;
+			}
+			if (flag) {
+				dllist_remove(s->solution, tmp);
+				memcpy(cur->data, &RR, s->solution->elem_size);
+			}
+		}
+		cur = cur->next;
+	}
+}
+
+static void rb_ra(stacks_t *s) {
 	dlnode_t *cur = s->solution->dummy->next;
 	enum instruction RR = rr;
 
@@ -89,7 +119,8 @@ static void ra_rra(stacks_t *s) {
 }
 
 void sol_optimize(stacks_t *s) {
-	to_rr(s);
+	ra_rb(s);
+	rb_ra(s);
 	ra_rra(s);
 	rra_ra(s);
 	return;
